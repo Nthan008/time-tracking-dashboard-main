dd32817f47952d094d16a7654183119671b7f2cc
Nthan008 <114546358+Nthan008@users.noreply.github.com>
2023-01-06 11:42:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function () {
+    $(".row-2").hide();
+    $("#day").hide();
+    $("#month").hide();
+    $("#btn1").click(function () {
+        $("#week").hide();
+        $("#month").hide();
+        $("#day").show();
+    });
+    $("#btn2").click(function () {
+        $("#week").show();
+        $("#month").hide();
+        $("#day").hide();
+    });
+    $("#btn3").click(function () {
+        $("#week").hide();
+        $("#month").show();
+        $("#day").hide();
+    });
+});
